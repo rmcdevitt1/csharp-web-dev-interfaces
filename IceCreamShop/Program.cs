61540f3baf663686489aa352d2b993440e1d5164
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-19 20:41:44 -0500

    adding

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,11 +11,9 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name' field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
